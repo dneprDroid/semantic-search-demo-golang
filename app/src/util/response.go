98d28c9be7cd201c3f3a8f717053450956651c15
user <ovechko.056@gmail.com>
2020-07-12 06:25:00 +0000

    Response util helpers

---
+++ app/src/util/response.go
@@ -0,0 +1,23 @@
+package util 
+
+import (
+	"net/http"
+	"encoding/json"
+)
+
+func RespWriteJson(w http.ResponseWriter, object interface{}) error {
+	response, err := json.Marshal(object)
+	if err != nil {
+		return err 
+	}
+	w.Write(response)
+	return nil 
+}
+
+func RespErrorStr(w http.ResponseWriter, errStr string) {
+	w.WriteHeader(http.StatusInternalServerError)
+	errMsg := map[string]string {
+		"error": errStr,
+	}
+	RespWriteJson(w, errMsg)
+}
